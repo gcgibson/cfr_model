ac76989a0a068cf447301017980099743007af93
graham gibson <gcgibson@umass.edu>
2021-02-12 14:43:38 -0700

    update

--- dir_post.R
+++ dir_post.R
@@ -14,4 +14,4 @@ time_to_death <- ggplot(df_,aes(x=x,y=rev(y)),fill=1) + geom_line() + theme_bw()
 
 time_to_death_total <- ggplot(df_full,aes(x=x,y=y,group=group)) + geom_line(alpha=.9) + theme_bw() + xlab("t") + ylab("P(T=t)")
 
-ggsave("/Users/gcgibson/cfr_numpyro/time_to_death",time_to_death,device = "png")
+ggsave("/Users/gcgibson/cfr_numpyro/time_to_death",time_to_death,device = "png",width=6,height=4)
